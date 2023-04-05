134ea2ebdfe65d79d2f066e841c520838c17305a
Victor Thompson <kelechithompson26@gmail.com>
2023-04-05 12:48:10 +0100

    add nextSequence-prototype

--- src/simon.js
+++ src/simon.js
@@ -3,3 +3,12 @@ export default function Simon() {
   this.sequenceUser = [];
   this.levelCounter = 0;
 }
+
+//This generates a random number and pushes it to the sequenceMade.
+Simon.prototype.nextSequence = function () {
+  let randomNumber = Math.floor(Math.random() * 4);
+  this.sequenceMade.push(randomNumber);
+  this.showSequence(this.sequenceMade[this.sequenceMade.length - 1]);
+  this.changeLevel();
+  this.sequenceUser = [];
+};
