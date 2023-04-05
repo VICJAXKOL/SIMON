f20cc1e0d20fe3da818b92adfb34f8191eee0b46
Victor Thompson <kelechithompson26@gmail.com>
2023-04-05 12:49:23 +0100

    add showSequence-prototype

--- src/simon.js
+++ src/simon.js
@@ -12,3 +12,34 @@ Simon.prototype.nextSequence = function () {
   this.changeLevel();
   this.sequenceUser = [];
 };
+
+//This displays the color  of each option
+Simon.prototype.showSequence = function (element) {
+
+  switch (element) {
+  case 0:
+    $("#green").addClass("dissapear");
+    setTimeout(function () {
+      $("#green").removeClass("dissapear");
+    }, 250);
+    break;
+  case 1:
+    $("#red").addClass("dissapear");
+    setTimeout(function () {
+      $("#red").removeClass("dissapear");
+    }, 250);
+    break;
+  case 2:
+    $("#yellow").addClass("dissapear");
+    setTimeout(function () {
+      $("#yellow").removeClass("dissapear");
+    }, 250);
+    break;
+  case 3:
+    $("#blue").addClass("dissapear");
+    setTimeout(function () {
+      $("#blue").removeClass("dissapear");
+    }, 250);
+    break;
+  }
+};
